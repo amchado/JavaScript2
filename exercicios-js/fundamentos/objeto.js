9ebc5aa00a3f11d356db08bb81e0d01c5b0a808e
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2023-03-15 17:06:30 -0300

    objetos

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,19 @@
+const prod1 = {}
+prod1.nome = 'Pc mega bolado'
+prod1.preco = 2000
+prod1['desconto'] = '10% off' // evitar atributos com espaços
+console.log(prod1)
+
+const prod2 = {
+     nome: 'Smarthphone',
+     valor: 3000,
+     desconto: '20% off'
+}
+console.log(prod2)
+
+
+
+const prod3 = {}
+prod3.nome = 'Console mediano'
+prod3.preco = 1350
+console.log(prod3)
